811c40ece908bf5a94f9beef121fd0726ce5d63b
Lucas Juárez <alphacentaurux@gmail.com>
2024-07-09 19:00:54 -0300

    Front de admin

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm navbar-light bg-light">
+<div class="container">
+  <a class="navbar-brand" href="index.html"> <img class="img-fluid" width="70px" src="img/logo-oscuro-sinfondo.png" alt="logo" </a>
+  <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavId">
+      <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Opciones</a>
+              <div class="dropdown-menu" aria-labelledby="dropdownId">
+                  <a class="dropdown-item" href="productos.html">Productos</a>
+                  <a class="dropdown-item" href="usuarios.html">Usuarios</a>
+                  <a class="dropdown-item" href="sesión.html">Cerrar Sesión</a>
+              </div>
+          </li>
+      </ul>
+      
+  </div>
+</div>
+</nav>
+`
