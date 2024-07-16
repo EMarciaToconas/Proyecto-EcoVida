4b3f523a8d2de0b26a19d5d2c656a1a38aa8dfa9
eli075 <elidelgado075@gmail.com>
2024-07-16 11:29:43 -0300

    correcion producto update

--- js/producto_editar.js
+++ js/producto_editar.js
@@ -11,7 +11,7 @@ const { createApp } = Vue
         stock:0,
         precio:0,
         descripcion:"",
-        categoria:0,
+        
         tipoproducto:0,
 
         //url:'https://mcerda.pythonanywhere.com/productos/'+id,
@@ -30,7 +30,6 @@ const { createApp } = Vue
                     this.stock=data.stock
                     this.precio=data.precio 
                     this.descripcion=data.descripcion
-                    this.categoria=data.categoria
                     this.tipoproducto=data.tipoproducto
                 })
                 .catch(err => {
@@ -45,7 +44,7 @@ const { createApp } = Vue
                 stock: this.stock,
                 imagen: this.imagen,
                 descripcion: this.descripcion,
-                categoria: this.categoria,
+                
                 tipoproducto: this.tipoproducto
             }
             console.log("Datos del producto a modificar:", producto); // Registro de los datos
